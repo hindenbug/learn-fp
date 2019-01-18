07e6044e2a91bb6f3c5b59797676df1d3815409d
hindenbug <manoj.mk27@gmail.com>
2019-01-18 17:43:56 +0100

    [elixir] add score correct and incorrect guess, refactor

--- elixir/hangman/lib/hangman/game.ex
+++ elixir/hangman/lib/hangman/game.ex
@@ -6,10 +6,14 @@ defmodule Hangman.Game do
     used: MapSet.new()
   )
 
-  def new_game() do
+  def new_game(word) do
     %Hangman.Game{
-      letters: Dictionary.random_word() |> String.codepoints()
+      letters: word |> String.codepoints()
     }
+  end
+
+  def new_game() do
+    new_game(Dictionary.random_word())
   end
 
   def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
@@ -27,9 +31,30 @@ defmodule Hangman.Game do
 
   def accept_move(game, guess, _already_guessed) do
     Map.put(game, :used, MapSet.put(game.used, guess))
+    |> score(Enum.member?(game.letters, guess))
   end
 
-  def tally(game) do
+  def score(game, _correct_guess = true) do
+    new_state =
+      MapSet.new(game.letters)
+      |> MapSet.subset?(game.used)
+      |> won?()
+
+    Map.put(game, :game_state, new_state)
+  end
+
+  def score(game = %{turns_left: 1}, _incorrect_guess) do
+    %{game | game_state: :lost, turns_left: 0}
+  end
+
+  def score(game = %{turns_left: turns_left}, _incorrect_guess) do
+    %{game | game_state: :incorrect_guess, turns_left: turns_left - 1}
+  end
+
+  def won?(true), do: :won
+  def won?(_), do: :correct_guess
+
+  def tally(_game) do
     4 + 4
   end
 end
--- elixir/hangman/test/game_test.exs
+++ elixir/hangman/test/game_test.exs
@@ -36,4 +36,51 @@ defmodule GameTest do
     {game, _tally} = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
+
+  test "a correct guess" do
+    game = Game.new_game("testword")
+
+    {game, _} = Game.make_move(game, "t")
+    assert game.game_state == :correct_guess
+    assert game.turns_left == 7
+  end
+
+  test "a correct word wins the game" do
+    moves = [
+      {"t", :correct_guess, 7},
+      {"e", :correct_guess, 7},
+      {"x", :incorrect_guess, 6},
+      {"s", :won, 6}
+    ]
+
+    game = Game.new_game("test")
+
+    Enum.reduce(moves, game, fn {guess, state, turns_left}, game ->
+      {game, _tally} = Game.make_move(game, guess)
+      assert game.game_state == state
+      assert game.turns_left == turns_left
+      game
+    end)
+  end
+
+  test "incorrect guesses looses the game" do
+    moves = [
+      {"x", :incorrect_guess, 6},
+      {"r", :incorrect_guess, 5},
+      {"6", :incorrect_guess, 4},
+      {"h", :incorrect_guess, 3},
+      {"i", :incorrect_guess, 2},
+      {"j", :incorrect_guess, 1},
+      {"k", :lost, 0}
+    ]
+
+    game = Game.new_game("test")
+
+    Enum.reduce(moves, game, fn {guess, state, turns_left}, game ->
+      {game, _tally} = Game.make_move(game, guess)
+      assert game.game_state == state
+      assert game.turns_left == turns_left
+      game
+    end)
+  end
 end
