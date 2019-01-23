60124fa37d19ab36a6f19ff226a64541f1330daf
hindenbug <manoj.mk27@gmail.com>
2019-01-23 13:00:54 +0100

    [elixir] Make hangman use GenServer

--- elixir/hangman/lib/hangman/game.ex
+++ elixir/hangman/lib/hangman/game.ex
@@ -33,7 +33,7 @@ defmodule Hangman.Game do
     }
   end
 
-  defp accept_move(game, guess, _already_guessed = true) do
+  defp accept_move(game, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
   end
 
@@ -70,7 +70,7 @@ defmodule Hangman.Game do
   end
 
   defp reveal_letter(letter, _in_word = true), do: letter
-  defp reveal_letter(letter, _not_in_word), do: "_"
+  defp reveal_letter(_letter, _not_in_word), do: "_"
 
   defp return_with_tally(game), do: {game, tally(game)}
 end
